b9bcb81b47d4c904fe8881e896abe057cac84c80
goodtho <Haloreach!!7>
2023-03-23 16:14:39 +0100

    Initial commit

---
+++ app/src/main/java/com/example/ba2023/MainActivity.kt
@@ -0,0 +1,18 @@
+package com.example.ba2023
+
+import android.app.Activity
+import android.os.Bundle
+import com.example.ba2023.databinding.ActivityMainBinding
+
+class MainActivity : Activity() {
+
+    private lateinit var binding: ActivityMainBinding
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+
+        binding = ActivityMainBinding.inflate(layoutInflater)
+        setContentView(binding.root)
+
+    }
+}
